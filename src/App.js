6a4bed76f351479c726d2fc8e6323750ef6298f2
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-01-10 14:07:04 +0530

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const App = () => {
+	const links = ['https://birthday-reminder09.netlify.app/']
 	return (
 		<div className='main-body'>
 			<h1>Basic React Projects</h1>
-			<button><a href="https://birthday-reminder09.netlify.app/">Birthday Reminder</a></button>
+			<button><a href={links[0]}>Birthday Reminder</a></button>
 		</div>
 	)
 }
