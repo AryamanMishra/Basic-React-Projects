8cdf6fb7a0eefc9db7a5e4ed67bbb0bb2862167a
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-01-16 13:53:51 +0530

    updated home page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react'
 const App = () => {
 	const links = [
 		'https://birthday-reminder09.netlify.app/',
-		'https://tours-info09.netlify.app/'
+		'https://tours-info09.netlify.app/',
+		'https://reviews09.netlify.app/'
 	]
 	return (
 		<div className='main-body'>
 			<h1>Basic React Projects</h1>
 			<button><a href={links[0]}>Birthday Reminder</a></button>
 			<button><a href={links[1]}>Tours Info</a></button>
+			<button><a href={links[2]}>Reviews</a></button>
 		</div>
 	)
 }
