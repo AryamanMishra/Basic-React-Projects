8fbe648e59249c6b314e520f0c7539517f697cd4
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-01-24 11:38:27 +0530

    resp added in bday-remind

--- all_projects/birthday-reminder/src/App.js
+++ all_projects/birthday-reminder/src/App.js
@@ -3,13 +3,18 @@ import {data} from './data'
 import List from './List'
 
 const App = () =>{
+
 	const [people,setPeople] = React.useState(data)
 	return (
-		<div className='main-body'>
-			<h1> {people.length} Birthdays Today </h1>
-			<List people={people}/>
-			<button onClick={()=> setPeople([])} className='allclear'>Clear All</button>
-		</div>
+		<main>
+			<div className='main-container'>
+				<div className='title'> 
+					{people.length === 1 ? <h2>{people.length} Birthday Today</h2> : <h2>{people.length} Birthdays Today</h2>}
+				</div>
+				<List people={people}/>
+				<button onClick={()=> setPeople([])} className='allclear'>Clear All</button>
+			</div>
+		</main>
 	)
 }
 
